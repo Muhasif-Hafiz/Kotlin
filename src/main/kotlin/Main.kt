6638011ed885c65b1bb7135b7abad230f2c24ee5
Muhasif Hafiz <muhasibhafeez91@gmail.com>
2024-10-01 02:53:00 +0530

    new Modules Added

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+//    var name = "Muhasib Hafeez "
+//    var address :Byte =32
+//    val age: Int = 21
+//    val height = 5.4F
+//    val isActive :Boolean =true
+
+//    println("Hello, $name!, your age is : $age\n your  ID 9os =and your height is $height\n Congrats!!  you are now active! : $isActive ")
+
+//    var MaximumInt : Int= Int.MAX_VALUE
+//    var minimumSize  : Int = Int.MIN_VALUE
+//    println("$MaximumInt")
+//    println("$minimumSize")
+
+    /*
+    * CONDITIONAL STATAMENTS
+    * */
+//    var age: Int = 25
+//   var text= if(age>=25) "Eligible" else "Not Eligible "
+//    println("$text")
+
+//}
+}
+
